26245e59da00eb3e6783bda031ab816a122e1899
Isaac Naylor <i.acnaylor@gmail.com>
2025-05-12 15:33:14 +0100

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import Constants from "expo-constants";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcG9sbG8tc2VydmVyIiwic3ViIjoiYzc4MTMxNmM5ZmY0YmVhYSIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjUwMDAiXSwiZXhwIjoxNzQ3NjU4Mzc3LCJpYXQiOjE3NDcwNTM1Nzd9.MwyZtCL6_q_NVC2qufUnt_3pTrzCJZyBM1tYBugnt3Y";
+
+function getBaseUrl(): string {
+  const ipAddr = Constants.expoConfig?.hostUri?.split(":").at(0);
+  return `http://${ipAddr}:1300`;
+}
+
 export default function App() {
+  const baseUrl = getBaseUrl();
+
+  const result = fetch(baseUrl + "/album/recommendation", {
+    headers: {
+      "Authorization": AUTH_TOKEN
+    }
+    }).then(res => res.json()).then(body => {
+      console.log(body);
+      return body;
+    });
+
+  console.log("Received Recommendation: ", result);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Response:</Text>
+      <Text>{JSON.stringify(result)}</Text>
       <StatusBar style="auto" />
     </View>
   );
