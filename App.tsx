f91c97639149b9250ddffee4146344b6af7f83ef
Isaac Naylor <i.acnaylor@gmail.com>
2025-05-12 15:41:43 +0100

    Removed hard-coded authtoken

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import Constants from "expo-constants";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcG9sbG8tc2VydmVyIiwic3ViIjoiYzc4MTMxNmM5ZmY0YmVhYSIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjUwMDAiXSwiZXhwIjoxNzQ3NjU4Mzc3LCJpYXQiOjE3NDcwNTM1Nzd9.MwyZtCL6_q_NVC2qufUnt_3pTrzCJZyBM1tYBugnt3Y";
+// Temporary for quick development.
+const AUTH_TOKEN = "insert_auth_token_here";
 
 function getBaseUrl(): string {
   const ipAddr = Constants.expoConfig?.hostUri?.split(":").at(0);
